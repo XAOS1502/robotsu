a596c1e4abf29dbb4531008b854b1eecda5e052e
「XΛOS」 <30846531+XAOS1502@users.noreply.github.com>
2017-08-22 07:52:14 +0530

    Update botsu.py
    Changed it because I added many more cogs, but some commands are left in here because of a reason: I'm lazy

--- botsu.py
+++ botsu.py
@@ -1,26 +1,33 @@
+import time
 import discord
+start_time = time.time()
 from discord.ext import commands
 
-import asyncio
-import random
+description = "Robotsu, Discord bot coded in Python by XΛOS and Lin²"
+bot_prefix = ['Robotsu ', 'robotsu ', '_', '?tag ']
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("Robotsu ", "robotsu ", "_", "?tag "), description=description)
+bot.remove_command('help')# ================ Because the help got too long
 
-description = "Robotsu alpha, coded in Python3 and released by XΛOS and Lin² for Discord"
-bot_prefix = ['Robotsu ', 'robotsu ', '_']
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("Robotsu ", "robotsu ", "_"), description=description)
-#bot.remove_command('help')
-#make sure to remove help after having created one
+# ================ cogs that are used
+bot.load_extension("cogs.interactive")
+bot.load_extension("cogs.misc")
+bot.load_extension("cogs.funtimes")
+bot.load_extension("cogs.misc")
+bot.load_extension("cogs.prediction")
+bot.load_extension("utils.administrative")
+bot.load_extension("utils.extracommands")
+bot.load_extension("cogs.mods")
+bot.load_extension("cogs.ayuda")
 bot.load_extension("cogs.dere")
-#loads the file <mycog> from the folder <cogs>
 
 @bot.event
-async def on_ready():
+async def on_ready():# ================ Does anybody read these?
     print(discord.__version__)
     print('     ε=ε=ε=ε=┏(;^o^)┛')
     print('誰かがこれを読んでいますか？')
-    print('ロボつ²ト名 : {} alpha' .format(bot.user.name))
-    print('クライアント ID : {}' .format(bot.user.id))
-    print('ロボつ²トバージョン : 0.3 「2017年 7月 21日」')
-    print('         ------')
+    print('ロボつ²ト名 : {} alpha'.format(bot.user.name))
+    print('クライアント ID : {}'.format(bot.user.id))
+    print('ロボつ²トバージョン : 0.6 「2017年 8月 21日」')
     print('クリエイター :「XΛOS」#1502')
     print('ロボつ²トがオンラインになりました 。。。')
     print(' ')
@@ -29,62 +36,50 @@ async def on_ready():
     print('-> ロードコア')
     print('--> ロボつ²ト同期が完了しました')
     print('---> ロボつ²トモード起動')
-    await bot.change_presence(game=discord.Game(name='type _ball'))
+    await bot.change_presence(game=discord.Game(type=0, name='with light'))
+    with open("logs/restart.txt", "a") as file:# ================ Useful, or not
+        file.write("\nRobotsu is very sorry he had to restart")
 
-#================================
-# Reaction Poll
-#================================
-@bot.command(aliases = ['ask', 'drawstraw'], name="poll", pass_context=True, description='Robotsu helps you ask other members, write :\nrobotsu poll <question or idea>')
-async def poll(ctx):
-        await bot.add_reaction(ctx.message, '👍')
-        await bot.add_reaction(ctx.message, '👎')
-        await bot.add_reaction(ctx.message, '🤷')
+# ================================
+# SERVER MANAGE COMMANDS
+# ================================
+@bot.command(pass_context=True, hidden=True)
+async def mute(ctx, user):
+    """Command to mute a user without kicking"""
+    text_overwrite = discord.PermissionOverwrite()
+    text_overwrite.send_messages = False
+    voice_overwrite = discord.PermissionOverwrite()
+    voice_overwrite.speak = False
+    for channel in ctx.message.server.channels:
+        if channel.type == discord.ChannelType.text:
+            await bot.edit_channel_permissions(channel, user, text_overwrite)
+        elif channel.type == discord.ChannelType.voice:
+            await bot.edit_channel_permissions(channel, user, voice_overwrite)
+        else:
+            continue
 
-#================================
-# Picks between multiple choices
-#================================
-@bot.command(aliases = ['choose', 'select'], description='Robotsu helps you choose between two or more things, separate them with a space')
-async def pick(*choices : str):
-    await bot.say(random.choice(choices))
+@bot.command(pass_context=True, hidden=True)
+async def unmute(self, ctx, user:discord.Member=None):
+    """Command to mute a user without kicking"""
+    text_overwrite = discord.PermissionOverwrite()
+    text_overwrite.send_messages = True
+    voice_overwrite = discord.PermissionOverwrite()
+    voice_overwrite.speak = True
+    for channel in ctx.message.server.channels:
+        if channel.type == discord.ChannelType.text:
+            await bot.edit_channel_permissions(channel, user, text_overwrite)
+        elif channel.type == discord.ChannelType.voice:
+            await bot.edit_channel_permissions(channel, user, voice_overwrite)
+        else:
+            continue
 
-#================================
-# SUPER BASIC calculator
-#================================
-@bot.command(aliases = ['+', 'sum', 'addition'], description='Robotsu alpha can add two numbers separated by space')
-async def add(left : int, right : int):#Adds two consecutive numbers
-    await bot.say(left + right)
-
-@bot.command(aliases = ['-', 'rest', 'subs'], description='Robotsu alpha can substract two numbers separated by space')
-async def substract(left : int, right : int):#Substracts two consecutive numbers
-    await bot.say(left - right)
-
-@bot.command(aliases = ['*', 'x', 'multiplicate'], description='Robotsu alpha can multiply two numbers separated by space')
-async def multiply(left : int, right : int):#Multiplies two consecutive numbers
-    await bot.say(left * right)
-
-@bot.command(aliases = ['/', 'into'], description='Robotsu alpha can divide two numbers separated by space')
-async def divide(left : int, right : int):#Divides two consecutive numbers
-    await bot.say(left / right)
-
-#================================
-# SERVER MANAGE
-#================================
-@bot.command(pass_context=True, name="mute", description="Robotsu helps to mute users without kicking or banning them")
-async def mute(ctx, member: discord.Member):
-    """Mutes the mentioned member in this channel."""
-    overwrite = discord.PermissionOverwrite()
-    overwrite.send_messages = False
-    await bot.edit_channel_permissions(ctx.message.channel, member, overwrite)
-    await asyncio.sleep(1)
-
-@bot.command(pass_context=True, name="kick", description="Robotsu can kick a member, they'll be able to join with an invite")
+@bot.command(pass_context=True, name="kick", hidden=True, description="Robotsu can kick a member, they'll be able to join with an invite")
 async def kick(ctx, user: discord.Member = None):
     """Kicks the mentioned member from the server."""
     if ctx.message.author.server_permissions.kick_members == True:
         if user != None:
             embed = discord.Embed(colour=0x5de316)
-            embed.add_field(name="­",
-                            value="{} has kicked {} for {}".format(ctx.message.author.mention, user.mention, reason))
+            embed.add_field(name="­", value="{0} has kicked {1} for {2}".format(ctx.message.author.mention, user.mention, reason))
             await bot.say(embed=embed)
             await bot.kick(user)
         else:
@@ -93,56 +88,103 @@ async def kick(ctx, user: discord.Member = None):
     else:
         await bot.say("You don't have permissions to kick anybody, try `kickme` command instead.")
 
-@bot.command(pass_context=True)
-async def ban(ctx, user: discord.Member=None):
-    if ctx.message.author.server_permissions.ban_members==True:
+@bot.command(pass_context=True, hidden=True)
+async def ban(ctx, user: discord.Member = None):
+    """Very self explanatory"""
+    if ctx.message.author.server_permissions.ban_members == True:
         await bot.ban(user)
     else:
         await bot.say("You have no permission to ban any users")
 
-#================================
-# TESTING RANDOM COMMANDS
-#================================
-@bot.command(aliases = ['seem', 'is'], pass_context=True, name='seems', description='Robotsu alpha is cute and adorable')
-async def seems(ctx):
-    await bot.say("Robotsu is cute")
-
-#========== bot commands extra labels
-@bot.command(pass_context=True, name='8ball', description='Robotsu alpha does not do 8balls')
-async def ball(ctx):
-    await bot.say("robotsu don't have 8ball, try `robotsu prediction`")
-
-@bot.command(aliases = ['fortune', 'predict', 'astrobotsu'], pass_context=True, name='prediction', description='Robotsu alpha will include future prediction forecast for your questions')
-async def prediction(ctx):
-    await bot.say("```Hello there,\nmy Dev XAOS is working hard to bring PREDICTION to you\nso you can enjoy asking questions about your future,\nthank you for your patience.```")
-#========== End of bot commands
-
-
-#================================
+# ================================
 # WELCOME AND FAREWELL MESSAGES
-#================================
+# ================================
 @bot.event
 async def on_member_join(member):
-    serverchannel = member.server.default_channel
+    serverchannel = member.server.default_channel# ================ It goes into the first General channel
     server = member.server
     count = len(server.members)
     msg = "**Welcome __{0}__ to {1}. We now have `{2}` members.**".format(member.mention, server, count)
-    await bot.say(serverchannel, msg)
+    await bot.send_message(serverchannel, msg)
 
 @bot.event
 async def on_member_remove(member):
-    serverchannel = member.server.default_channel
+    serverchannel = member.server.default_channel# ================ It goes into the first General channel
     server = member.server
     count = len(server.members)
-    msg = "**And now, {0}'s watch has ended.\nHold the door, Hodor.\nWe are left with {1} members in {2}**.".format(member.mention, count, server)
-    await bot.say(serverchannel, msg)
+    msg = "**And now, {0}'s watch has ended.\nHold the door, Hodor.\nWe are left with {1} members in {2}**.".format(
+        member.mention, count, server)
+    await bot.send_message(serverchannel, msg)
 
-#================ Says when a member joined
-@bot.command(description="Robotsu finds out when a member joined the Discord guild")
-async def joindate(member : discord.Member):
+# ================ Says when a member joined
+@bot.command(hidden=True, description="Robotsu finds out when a member joined the Discord guild")
+async def joindate(member: discord.Member):
     await bot.say("{0.name} joined in {0.joined_at}".format(member))
 
+# ================ Because it is good to remember who helped
+@bot.command(pass_context=True, name="credits", description="Robotsu alpha is brought to you because of the following support")
+async def credits(ctx):
+    embed = discord.Embed(title="Robotsu Beta version 0.6 - Credits", colour=discord.Colour(0x2196f3), description="**ロボつ²** is the first Bot made by 「XΛOS」#1502, but it has grown from **alpha stage** to **beta** with the support and guidance of many expert individuals.")
+    embed.add_field(name="Thanks to:", value="Maximilian\nMiguel F\nZey\nMei\nTyler\nLesbisch™\nLin Lin\nAndrija", inline=True)
+    embed.add_field(name=":small_orange_diamond:", value="**Irae/Clockwork#9860\nAnthsoul#6929\nzeyla#5479\nmei#5429\nTrain#1115\nLesbisch#8111\nLin²#5427**\nRetardedKid#2405", inline=True)
+    await bot.say(embed=embed)
 
-#===== Beginning of secret token
-bot.run('token')
-#===== End of secret token
+# ================ Simple command to rend notes to self
+@bot.event
+async def on_message(message):
+    if message.content.startswith.lower()("note to self"):
+        await bot.say('Note sent in a DM!')
+        await bot.send_message(message.author, message.content.replace('note to self ', ''))
+    await bot.process_commands(message)
+
+# ================ Want to have a message in Embed?
+@bot.command(pass_context=True, name="embed")
+async def embed(ctx, *, message:str):
+    """Command for Embedding your text
+    it deletes your original message
+    and quotes your name"""
+    try:
+        await bot.delete_message(ctx.message)
+    except:
+        pass
+    embed = discord.Embed(title="{0} wrote:".format(ctx.message.author.display_name), colour=discord.Colour(0x2a6e81), description="{0}".format(message))
+    await bot.say(embed=embed)
+
+# ================================
+# MESSAGE ON COMMAND ERROR
+# ================================
+@bot.event
+async def on_command_error(error, ctx):
+    if isinstance(error, commands.CommandNotFound):
+        return
+    if isinstance(error, commands.DisabledCommand):
+        await bot.send_message(ctx.message.channel, "This command has been disabled")
+        return
+    if isinstance(error, checks.dev_only):
+        await bot.send_message(ctx.message.channel, "This command can only be ran by the bot developers")
+        return
+    if isinstance(error, checks.owner_only):
+        await bot.send_message(ctx.message.channel, "This command can only be ran by the bot owner")
+        return
+    if isinstance(error, checks.not_nsfw_channel):
+        await bot.send_message(ctx.message.channel, "This command can only be ran in NSFW enabled channels. It must either be named `nsfw` or the name must start with `nsfw-`")
+        return
+    if isinstance(error, checks.not_server_owner):
+        await bot.send_message(ctx.message.channel, "Only the server owner (`{}`) can use this command".format(ctx.message.server.owner))
+        return
+    if isinstance(error, checks.no_permission):
+        await bot.send_message(ctx.message.channel, "You do not have permission to use this command".format(ctx.message.server.owner))
+        return
+    if ctx.message.channel.is_private:
+        await bot.send_message(ctx.message.channel, "An error occured while trying to run this command, this is most likely because it was ran in a private message channel. Please try running this command on a server.")
+        return
+
+    # In case the bot failed to send a message to the channel, the try except pass statement is to prevent another error
+    try:
+        await bot.send_message(ctx.message.channel, error)
+    except:
+        pass
+    log.error("An error occured while executing the {} command: {}".format(ctx.command.qualified_name, error))
+
+#===== Token
+bot.run('TOKEN_GOES_HERE_BUT_DO_NOT_SHARE_IT')
